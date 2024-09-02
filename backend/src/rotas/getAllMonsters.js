cd72d3e1b62adade9e57a273ff9b4a3895481eb9
analulu <analuiza.aljc@gmail.com>
2024-09-02 15:51:18 -0300

    getAllMonsters

--- backend/src/rotas/getAllMonsters.js
+++ backend/src/rotas/getAllMonsters.js
@@ -1,11 +1,21 @@
 const express = require("express")
+const prismaClient = require("../prisma/client.js")
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    const id = req.params.id
+router.get("/", async (req, res) => {
+    
+    const allMonsters = await prismaClient.monster.findMany()
 
-    res.json({messagem: `${id}`})
+    if (!allMonsters){
+        res.json({message: "Nada encontrado"})
+
+    } else{
+
+        res.json(allMonsters)
+    }
+
+    
 })
 
 module.exports = router
