2fd33303bf7360f89b27fd8cc78ca6d6adf07d4b
Bruna Heloisa <brunaheloisa@alunos.utfpr.edu.br>
2024-09-09 10:54:41 -0300

    Update MonsterService.jsx

--- app/src/Components/MonsterService.jsx
+++ app/src/Components/MonsterService.jsx
@@ -4,19 +4,19 @@ export const fetchMonsters = async () => {
   try {
     const response = await axios.get("http://localhost:3000/monsters/");
     const data = response.data;
-    return data.results.map(monster => monster.index); // Mapeia pelo id (index)
+    return data.results.map(monster => monster.name); // Mapeia pelo id (index)
   } catch (error) {
     throw new Error("Falha ao carregar os dados dos monstros.");
   }
 };
 
-export const searchMonster = async (index) => {
+export const searchMonster = async (name) => {
   if (name === "") {
     throw new Error("Digite o nome do monstro!!!");
   }
 
   try {
-    const response = await axios.get(`http://localhost:3000/monsters/${index}`);
+    const response = await axios.get(`http://localhost:3000/monsters/${name}`);
     const data = response.data;
     return {
       name: data.name,
