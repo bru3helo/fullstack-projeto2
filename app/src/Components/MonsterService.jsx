11dd47e5d5f7c329e5166f6f38ab42749d15912b
Bruna Heloisa <brunaheloisa@alunos.utfpr.edu.br>
2024-09-09 11:39:49 -0300

    Update MonsterService.jsx

--- app/src/Components/MonsterService.jsx
+++ app/src/Components/MonsterService.jsx
@@ -1,31 +1,38 @@
-import axios from "axios";
+
 
 export const fetchMonsters = async () => {
   try {
-    const response = await axios.get("http://localhost:3000/monsters/");
-    const data = response.data;
-    return data.results.map(monster => monster.name); // Mapeia pelo id (index)
+    const response = await fetch("http://localhost:3000/monsters");
+    if (!response.ok) {
+      throw new Error("falha ao buscar!");
+    }
+    const data = await response.json();
+    return data.results.map(monster => monster.name);
   } catch (error) {
-    throw new Error("Falha ao carregar os dados dos monstros.");
+    throw new Error("Falha ao carregar os dados dos montros.");
   }
 };
 
 export const searchMonster = async (name) => {
-  if (name === "") {
-    throw new Error("Digite o nome do monstro!!!");
-  }
-
   try {
-    const response = await axios.get(`http://localhost:3000/monsters/${name}`);
-    const data = response.data;
-    return {
-      name: data.name,
-      type: data.type,
-      size: data.size,
-      languages: data.languages,
-      alignment: data.alignment
-    };
+    const response = await fetch(`http://localhost:3000/monsters/${name}`);
+    if (!response.ok) {
+      throw new Error("Verifique novamente o nome do monstro!!!");
+    }
+    if(name === ""){
+      throw new Error("Digite o nome do monstro!!!");
+    }else{
+      const data = await response.json();
+      return {
+        name: data.name,
+        type: data.type,
+        size: data.size,
+        languages: data.languages,
+        alignment: data.alignment
+      };
+    }
+
   } catch (error) {
-    throw new Error(`Não foi possível encontrar o monstro: ${error.message}`);
+    throw new Error(`Não foi possivel encontrar, ${error.message}`);
   }
 };
